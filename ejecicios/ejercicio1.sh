ba7e5f7b1b6ddeb682cda1df605d3dd5c58f5dd7
Matias .M <mendoza.e.matias@gmail.com>
2025-10-02 17:00:45 -0300

    advance

--- ejecicios/ejercicio1.sh
+++ ejecicios/ejercicio1.sh
@@ -3,13 +3,22 @@
 read -p "Ingresa el nombre del nuevo usuario " user
 read -p "Ingresa el nombre del nuevo grupo " group
 
-if [ $(grep -c "$group" /etc/group) -eq 0 ]
+if [ $(grep -c "$group" /etc/group ) -eq 0 ]
 then
        sudo groupadd $group 
 
-       sudo useradd -m -d /home/$user/ -s /bin/bash -g $group -G sudo $user
+       sudo useradd -m -d /home/$user/ -s /bin/bash -g $group -G sudo  $user 
 	
        sudo passwd $user
 else
 	echo The group - $group - already exist
 fi
+	
+	 sudo mkdir /home/$user/proyectos
+	 sudo chmod 777 /home/$user/proyectos
+	 sudo touch /home/$user/proyectos/registro_usuario02.txt
+	 sudo chown -R $user:$group /home/$user/proyectos
+	 sudo ls -l /home/$user/proyectos
+    	 sudo cp -r /etc/skel/ /home/$user/proyectos 
+	 echo hola > /home/$user/proyectos/registro_usuario2.txt
+	 sudo cat /home/$user/proyectos/registro_usuario2.txt
